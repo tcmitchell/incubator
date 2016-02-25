f0b5e6ada4ef97fe1866ddbf25473c25804ed413
Tom Mitchell <tmitchell@bbn.com>
2016-02-25 13:42:47 -0500

    Add is_available queries
    Still a work in progress. There's a bug in the JSON query that shows
    aggregates as unavailable ('false') when the XML format shows some as
    available and some as not available.

--- geni-monitoring/README.md
+++ geni-monitoring/README.md
@@ -1,3 +1,5 @@
+# Example Queries
+
 1. Get a list of aggregates in JSON format
 
     http://genimondev.uky.edu/API/info/?q={"obj":{"type":"aggregate"},"output":"json","infoType":"simple"}
@@ -21,3 +23,23 @@
 6. Get information about a single aggregate by URN in XML format
 
     http://genimondev.uky.edu/API/info/?q={"obj":{"id":["urn:publicid:IDN+instageni.maxgigapop.net+authority+cm"],"type":"aggregate"},"output":"xml","infoType":"simple"}
+
+7. Get event information about an aggregate
+
+    http://genimondev.uky.edu/API/data/?q={"obj":{"id":["max-ig"],"type":"aggregate"},"output":"xml","ts":{"gte":1456260153962,"lt":1456263753962}}
+
+8. Just is_available for an aggregate
+
+    http://genimondev.uky.edu/API/data/?q={"eventType":["is_available"],"obj":{"id":["max-ig"],"type":"aggregate"},"output":"xml","ts":{"gte":1456345005000,"lt":1456348605000}}
+
+ * I couldn't get "output":"json" to work with this query, the document was empty
+
+# Slivers
+
+1. List all slivers in XML
+
+    http://genimondev.uky.edu/API/info/?q={"obj":{"type":"sliver"},"output":"xml","infoType":"simple"}
+
+2. Info about a single sliver in json
+
+    http://genimondev.uky.edu/API/info/?q={"obj":{"id":["lan.sdn.uky.edu_sliver_38337"],"type":"sliver"},"output":"json","infoType":"detailed"}
--- geni-monitoring/aggmon.py
+++ geni-monitoring/aggmon.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python
 
+# Monitoring API: http://groups.geni.net/geni/wiki/GENIMonitoring/API
+
 import urllib2
-url = 'http://genimondev.uky.edu/API/info/?q={"obj":{"type":"aggregate"},"output":"json","infoType":"simple"}'
+import json
+import datetime
+import time
+import pprint
+
+BASE_URL = 'http://genimondev.uky.edu/API'
+INFO_URL = '%s/info/' % (BASE_URL)
+DATA_URL = '%s/data/' % (BASE_URL)
+
+# TODO: Still need to HTTP encode the resulting query
+def buildMonQuery(**kwargs):
+    return json.dumps(kwargs, separators=(',', ':'))
+
+def getIsAvailableEvents(agg_list):
+    # http://genimondev.uky.edu/API/data/?q={"eventType":["is_available"],"obj":{"id":["max-ig"],"type":"aggregate"},"output":"xml","ts":{"gte":1456345005000,"lt":1456348605000}}
+    end = datetime.datetime.utcnow()
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(minutes=60)
+    end_ts = int(time.mktime(end.timetuple()) * 1000)
+    start_ts = int(time.mktime(start.timetuple()) * 1000)
+    query = buildMonQuery(eventType=['is_available'],
+                          obj={'id':agg_list,
+                               'type': 'aggregate'},
+                          output='json',
+                          ts={'gte':start_ts,
+                              'lt':end_ts})
+    url = DATA_URL + '?q=' + query
+    print url
+    f = urllib2.urlopen(url)
+    all = f.read()
+    try:
+        return json.loads(all)
+    except:
+        raise
+
+
+# Get a list of aggregates from the monitoring server
+url = INFO_URL + '?q={"obj":{"type":"aggregate"},"output":"json","infoType":"simple"}'
 f = urllib2.urlopen(url)
 all = f.read()
-import json
-
 x = json.loads(all)
 
 def getMonVal(obj, field):
@@ -21,12 +58,40 @@ def getMonTS(obj, field):
         return 0
 
 for a in x:
+    agg_urn = getMonVal(a, "urn")
+    # Getting is_available events by URN does not appear to work
+    # by_urn = getIsAvailableEvents([agg_urn])
+    agg_id = getMonVal(a, "id")
+    by_id = getIsAvailableEvents([agg_id])
+    #print "By ID:"
+    #pprint.pprint(by_id)
+    # Data is wrapped in an extra list
+    by_id = by_id[0]
+    if not by_id:
+        print agg_urn
+        print "\tNo Data"
+        continue
+    agg_data = by_id[0]
+    ts_data = agg_data['tsdata']
+    #print ts_data
+    available = False
+    available_ts = 0
+    for ts in ts_data:
+        if ts['ts'] > available_ts:
+            available_ts = ts['ts']
+            available = ts['v']
+    print agg_urn
+    pretty_time = time.strftime('%c', time.localtime(available_ts/1000))
+    print "\t%s: %r" % (pretty_time, available)
+
+for a in x:
+    break
     agg_id = getMonVal(a, "id")
     agg_urn = getMonVal(a, "urn")
     ts = getMonTS(a, "ts")
     polled = getMonTS(a, "last_polled_by_collector")
     mapped = getMonTS(a, "last_mapped_by_collector")
-    print "\tid: %s" % (agg_id)
+    print "id: %s" % (agg_id)
     print "\tts: %d" % (ts)
     print "\tpolled: %d" % (polled)
     print "\tmapped: %d" % (mapped)

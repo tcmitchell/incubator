22a55458465f009bebe3c6b41002d2ac620722fc
Tom Mitchell <tmitchell@bbn.com>
2016-07-29 12:54:24 -0400

    Finish first version of MDQ mirror
    Mirrors the InCommon MDQ beta server by downloading each entityID.
    A sha1 hash symlink is created to each entityID's data to comply
    with the SAML profile.

--- mdq/mdq-mirror.py
+++ mdq/mdq-mirror.py
@@ -1,11 +1,15 @@
 import hashlib
 import json
+import os
+import time
 import urllib
 import urllib2
 
 
 ENTITY_LIST_URL = 'http://mdq-beta.incommon.org/global/x-entity-list'
-MDQ_BASE = 'http://mdq-beta.incommon.org/global/'
+
+# Note: leave off the trailing slash, a slash will be appended
+MDQ_BASE = 'http://mdq-beta.incommon.org/global'
 
 # Fetch the entity list
 # JSON decode it
@@ -15,7 +19,6 @@ MDQ_BASE = 'http://mdq-beta.incommon.org/global/'
 #       Put the data in the file
 #       Generate a SHA1 hash of the entityID
 #       Create a symlink from the hash to the file
-
 
 
 def hash_filename(entityID):
@@ -28,24 +31,37 @@ def entity_filename(entityID):
     return entityID.replace('/', '%2F')
 
 
-def fetch_metadata(base, entityID):
-    pass
-
-
-def fetch_all_entities(url):
+def fetch_url(url):
     x = urllib2.urlopen(url)
     raw_data = x.read()
     x.close()
+    return raw_data
+
+
+def fetch_metadata(base, entityID):
+    quoted_entity = urllib.quote(entityID, safe='')
+    url = '/'.join([base, 'entities', quoted_entity])
+    print url
+    return fetch_url(url)
+
+
+def fetch_json(url):
+    raw_data = fetch_url(url)
     data = json.loads(raw_data)
     return data
 
 
 def main():
-    all_entities = fetch_all_entities(ENTITY_LIST_URL)
+    all_entities = fetch_json(ENTITY_LIST_URL)
     for entity in all_entities:
         entityID = entity['entityID']
         e_fname = entity_filename(entityID)
         h_fname = hash_filename(entityID)
         print "%s\t%s\t%s\t" % (entityID, e_fname, h_fname)
+        metadata = fetch_metadata(MDQ_BASE, entityID)
+        with open(e_fname, 'wb') as f:
+            f.write(metadata)
+        os.symlink(e_fname, h_fname)
+        time.sleep(5)
 
 main()
